291d3acceed662824a2c30048168cf07dfa5b2b7
mikzhang <zlanxs@163.com>
2018-04-24 21:05:12 +0800

    'fix_pic_display'

--- source/_posts/楞严经/write_blog.py
+++ source/_posts/楞严经/write_blog.py
@@ -20,7 +20,8 @@ def get_pic_files(root_dir):
     return pic_files
 
 def md_pic_link(f):
-    pic_link = '{%% asset_img %s %%}' % f
+    #pic_link = '{%% asset_img %s %%}' % f
+    pic_link = '![%s](楞严经/%s)' % (f,f)
     return pic_link
 
 pic_files = get_pic_files(root_dir)
@@ -39,5 +40,3 @@ with open(os.path.join(parent_path, '楞严经.md'), 'w') as mdf:
     mdf.write('\n')
     for i in range(0, len(pic_files)):
         mdf.write(md_pic_link(pic_files[i])+'\n')
-
-
